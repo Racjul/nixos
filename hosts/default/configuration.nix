13ab58366d179058e3cfa535be51b46fc16d8032
Racjul <racjul2003@icloud.com>
2024-05-02 15:08:43 -0400

    Ajout waybar

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -8,9 +8,9 @@
     imports =
         [ # Include the results of the hardware scan.
         ./hardware-configuration.nix
-        inputs.home-manager.nixosModules.default
+            inputs.home-manager.nixosModules.default
         ];
-    
+
 # Bootloader
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
@@ -22,104 +22,106 @@
     services.displayManager.sddm.wayland.enable = true;
 
 # Enable networking
-networking.networkmanager.enable = true;
+    networking.networkmanager.enable = true;
 
 # Set your time zone.
-time.timeZone = "America/Toronto";
+    time.timeZone = "America/Toronto";
 
 # Select internationalisation properties.
-i18n.defaultLocale = "en_CA.UTF-8";
+    i18n.defaultLocale = "en_CA.UTF-8";
 
 # Enable CUPS to print documents.
-services.printing.enable = true;
+    services.printing.enable = true;
 
 # Enable sound with pipewire.
-sound.enable = true;
-hardware.pulseaudio.enable = false;
-security.rtkit.enable = true;
-services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-
-};
-
-
-users.users.julienracette = {
-    isNormalUser = true;
-    description = "JulienRacette";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-        firefox-wayland
-        thunderbird
-        spotify
-        discord
-    ];
-};
-home-manager = {
-    extraSpecialArgs = {inherit inputs;};
-    users ={
-        "julienracette" = import ./home.nix;
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
 
     };
 
-};
+    users.users.julienracette = {
+        isNormalUser = true;
+        description = "JulienRacette";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+            firefox-wayland
+                thunderbird
+                spotify
+                discord
+        ];
+    };
+    home-manager = {
+        extraSpecialArgs = {inherit inputs;};
+        users ={
+            "julienracette" = import ./home.nix;
+
+        };
+
+    };
 
 # Allow unfree packages
-nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
 # Allow Flakes and nix-command
-nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.settings.experimental-features = ["nix-command" "flakes"];
 
 # Installed Packages
-environment.systemPackages = with pkgs; [
-    home-manager
-    nh
+    environment.systemPackages = with pkgs; [
+        home-manager
+            nh
 
-    # wayland / hyprland dependencies
-    hyprland
-    hyprpaper
-    waybar
-    rofi-wayland
-    wl-clipboard
+# wayland / hyprland dependencies
+            hyprland
+            hyprpaper
+            rofi-wayland
+            wl-clipboard
+            hyprpicker
 
-    # Notification
-    dunst
-    libnotify
-    
-    # basic terminal tools
-    wget
-    unzip
-    git
-    kitty
-    neovim
+# Notification
+            dunst
+            libnotify
 
-    # For bluetooth and wifi:
-    networkmanagerapplet
-    blueman
+# basic terminal tools
+            wget
+            unzip
+            git
+            kitty
+            neovim
 
-    # Packages Manager / Installer
-    kdePackages.qt6ct
-    qt6.full
-    gnumake
-    nodejs_21
-    cargo
-];
+# For bluetooth and wifi:
+            networkmanagerapplet
+            blueman
+            mpd
+
+# Packages Manager / Installer
+            kdePackages.qt6ct
+            nodejs_21
+            cargo
+            python3
+            nil
+            playerctl
+            pavucontrol
+            ];
 
 
 # Before changing this value read the documentation for this option
-system.stateVersion = "23.11"; # Did you read the comment?
+    system.stateVersion = "23.11"; # Did you read the comment?
 
 # Enable Hyprland 
-programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-};
+        programs.hyprland = {
+            enable = true;
+            xwayland.enable = true;
+        };
 
-xdg.portal.enable = true;
-xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-environment.sessionVariables.NIX_OZONE_WL = "1";
+    environment.sessionVariables.NIX_OZONE_WL = "1";
 }
--- modules/home-manager/configs.nix
+++ modules/home-manager/configs.nix
@@ -1,13 +1,18 @@
 {pkgs,lib,config, ... }:{
     imports = [
+        #Terminal modules
         ./terminal/fish.nix
         ./terminal/starship.nix
         ./terminal/cli.nix
+
+        ./hyprland/waybar.nix
+        
     ];
     config = {
         starship.enable = true;
         fish.enable = true;
         cli.enable =true;
+        waybar.enable = true;
     };
 }
 
